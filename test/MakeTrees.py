1cc87ff26bb025ef3aef3afd638680fcfadca8e4
Rafael <rateixei@cern.ch>
2015-08-14 18:07:22 +0200

    Style

--- test/MakeTrees.py
+++ test/MakeTrees.py
@@ -2,6 +2,7 @@ import FWCore.ParameterSet.Config as cms
 from flashgg.bbggTools.microAOD_RadFiles import *
 from flashgg.bbggTools.microAOD_GravFiles import *
 from flashgg.bbggTools.More_microAOD_DJet40Inf import *
+from flashgg.bbggTools.pColors import *
 
 ##### Arguments
 import flashgg.bbggTools.VarParsing as opts
@@ -50,11 +51,21 @@ process.source = cms.Source("PoolSource",
 
 process.load("flashgg.bbggTools.bbggTree_cfi")
 process.bbggtree.OutFileName = cms.untracked.string(outputFile)
-print "is it doing selection?",options.doSelection
+
+print bcolors.OKBLUE + "#####################################################################" + bcolors.ENDC
+print bcolors.OKBLUE + " _   _  _   _  _      _                     _____                    " + bcolors.ENDC
+print bcolors.OKBLUE + "| | | || | | || |    | |                   |_   _|                   " + bcolors.ENDC
+print bcolors.OKBLUE + "| |_| || |_| || |__  | |__    __ _   __ _    | |   _ __   ___   ___  " + bcolors.ENDC
+print bcolors.OKBLUE + "|  _  ||  _  || '_ \ | '_ \  / _` | / _` |   | |  | '__| / _ \ / _ \ " + bcolors.ENDC
+print bcolors.OKBLUE + "| | | || | | || |_) || |_) || (_| || (_| |   | |  | |   |  __/|  __/ " + bcolors.ENDC
+print bcolors.OKBLUE + "\_| |_/\_| |_/|_.__/ |_.__/  \__, | \__, |   \_/  |_|    \___| \___| " + bcolors.ENDC
+print bcolors.OKBLUE + "                              __/ |  __/ |                           " + bcolors.ENDC
+print bcolors.OKBLUE + "                             |___/  |___/                            " + bcolors.ENDC
+print bcolors.OKBLUE + "#####################################################################" + bcolors.ENDC
+print bcolors.FAIL + "Is it performing the analysis selection?",options.doSelection, bcolors.ENDC
 if options.doSelection is True:
 	process.bbggtree.doSelection = cms.untracked.uint32(1)
 if options.doSelection is False:
-	print "TEST"
 	process.bbggtree.doSelection = cms.untracked.uint32(0)
 
 process.p = cms.Path(process.bbggtree)
