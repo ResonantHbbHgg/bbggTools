4dd80e7f3b99a539970e3fce1c9a393b1f2bf0b9
Rafael <rateixei@cern.ch>
2016-02-18 12:52:55 +0100

    moved boost to the correct place

--- Compile.sh
+++ Compile.sh
@@ -1,1 +1,1 @@
-USER_CXXFLAGS="-Wno-delete-non-virtual-dtor -Wno-error=unused-but-set-variable -Wno-error=unused-variable -lboost_program_options" scramv1 b -j 10
+USER_CXXFLAGS="-Wno-delete-non-virtual-dtor -Wno-error=unused-but-set-variable -Wno-error=unused-variable" scramv1 b -j 10
